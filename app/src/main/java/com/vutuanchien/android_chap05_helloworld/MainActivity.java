a5fc8c61196a8f85fefc46ea1ef43d4edfa70b30
VuTuanChien <jbtuanchien.11@gmail.com>
2016-03-09 09:59:32 +0700

    Android_Chap05_HelloWorld
    Android_Chap05_HelloWorld

---
+++ app/src/main/java/com/vutuanchien/android_chap05_helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vutuanchien.android_chap05_helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //    Called when the activity is first created.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
